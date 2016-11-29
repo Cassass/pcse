29fa3be9fe9bf3bd5eb88eb06c2e182d5590b4b0
Fabien Cassassolles <cassassf@ensipcserveur.imag.fr>
2016-11-29 22:06:14 +0100

    start.c

--- start.c
+++ start.c
@@ -1,21 +1,38 @@
 #include <cpu.h>
 #include <inttypes.h>
 #include "time_handler.h"
-
+#include "gestion_processus.h"
 extern void traitant_IT_32(void);
-
-// on peut s'entrainer a utiliser GDB avec ce code de base
-// par exemple afficher les valeurs de x, n et res avec la commande display
 
 
 void kernel_start(void)
 {
-	// initialisation
-	init_traitant_IT(32, traitant_IT_32);
-	set_clock();
-	masque_IRQ(0, false);
+	// initialisation des structures de processus
+	init_processus();
+	/* // initialisation de idle */
+	/* processus pidle; */
+	/* pidle.pid = 0; */
+	/* pidle.name = "idle"; */
+	/* pidle.state = ELU; */
+	
+	/* // initialisation de proc1 */
+	/* //ptr de fonction pour la pile */
+	/* void (*func_ptr)(void); */
+	/* func_ptr = proc1; */
+
+	/* processus proce1; */
+	/* proce1.pid = 1; */
+	/* proce1.name = "proc1"; */
+	/* proce1.state = ACTIVABLE; */
+	/* proce1.stack[511] =  (uint32_t)(func_ptr); */
+	/* proce1.proc_reg[1] = (uint32_t)&proce1.stack[511]; */
+	// lancement du test
+	idle();
+	/* init_traitant_IT(32, traitant_IT_32); */
+	/* set_clock(); */
+	/* masque_IRQ(0, false); */
 	//demasquage des interruptions externes
-	sti();
+	//sti();
 	while (1) {
 		// cette fonction arrete le processeur
 		hlt();
